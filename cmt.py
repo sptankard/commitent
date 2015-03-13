5932a4d9c0e2b71aa3cb7f708f74203594dff1f8
Scott Tankard <sptankard@gmail.com>
2015-03-13 15:19:55 -0700

    some kinda change

--- cmt.py
+++ cmt.py
@@ -5,15 +5,19 @@ from plasTeX.TeX import TeX
 #from plasTeX.Renderers.XHTML import Renderer
 #Renderer().render(TeX(file=sys.argv[-1]).parse())
 
-doc = TeX(file=sys.argv[-1]).parse()
+doc = TeX()
+
+doc.input(open(sys.argv[-1], 'r'))
+
 for i in doc:
     print i
+    
 
+#doc = TeX(file=sys.argv[-1]).parse()
 
-
-  tex = TeX()
-    tex.input(open('foo.tex','r'))
-    for token in tex:
-        print token
+#tex = TeX()
+#tex.input(open('foo.tex','r'))
+#for token in tex:
+#    print token
 
         
